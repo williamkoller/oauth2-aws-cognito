4b59c93b600848d3a068abd47a91f7f3381356ba
william koller <wkoller25@gmail.com>
2020-12-09 18:38:22 -0300

    feat: add providers jwt strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
-import { AuthConfig } from './auth.config'
+import { AuthConfig } from './config/auth.config'
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
+import { JwtStrategy } from '../auth/strategy/jwt.strategy'
 
 @Module({
-  providers: [AuthService, AuthConfig],
+  providers: [AuthService, AuthConfig, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
