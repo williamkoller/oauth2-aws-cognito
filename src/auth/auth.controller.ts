14e2e2b4fbe31255970422fb7a7cf856fc2bc706
william koller <wkoller25@gmail.com>
2020-12-09 18:38:49 -0300

    refactor: alter type return

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,25 +1,35 @@
 import { BadRequestException, Body, Controller, Post } from '@nestjs/common'
 import { AuthService } from './auth.service'
+import { AuthCredentialsDto } from './dtos/auth-credentials.dto'
+import { AuthRegisterDto } from './dtos/auth-register.dto'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  async register(@Body() registerRequest: { name: string; email: string; password: string }) {
+  async register(@Body() authRegisterRequest: AuthRegisterDto) {
     try {
-      return this.authService.registerUser(registerRequest)
-    } catch (error) {
-      throw new BadRequestException(error.message)
+      if (
+        authRegisterRequest.password.length < 8 ||
+        !/[a-z]/.test(authRegisterRequest.password) ||
+        !/[A-Z]/.test(authRegisterRequest.password) ||
+        !/[0-9]/.test(authRegisterRequest.password)
+      ) {
+        throw new BadRequestException('Password requirements not met.')
+      }
+      return this.authService.registerUser(authRegisterRequest)
+    } catch (e) {
+      throw new BadRequestException(e.message)
     }
   }
 
   @Post('login')
-  async login(@Body() authenticateRequest: { name: string; password: string }) {
+  async login(@Body() authenticateRequest: AuthCredentialsDto) {
     try {
       return await this.authService.authenticateUser(authenticateRequest)
-    } catch (error) {
-      throw new BadRequestException(error.message)
+    } catch (e) {
+      throw new BadRequestException(e.message)
     }
   }
 }
