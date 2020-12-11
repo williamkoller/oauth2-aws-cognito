1e26be4d1a814f5b15b20a1e43fe2d2a3289bd2d
william koller <wkoller25@gmail.com>
2020-12-11 11:43:06 -0300

    feat: add PassportModule defount jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,8 +3,10 @@ import { AuthConfig } from './config/auth.config'
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
 import { JwtStrategy } from '../auth/strategy/jwt.strategy'
+import { PassportModule } from '@nestjs/passport'
 
 @Module({
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
   providers: [AuthService, AuthConfig, JwtStrategy],
   controllers: [AuthController],
 })
